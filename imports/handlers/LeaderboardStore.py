c4ba679d228a407e6a5b31750ef31a361fe62e70
Jeremy Westwood <jeremy@j5int.com>
2015-07-27 17:03:10 +0200

    Calculate Rating change correctly

--- imports/handlers/LeaderboardStore.py
+++ imports/handlers/LeaderboardStore.py
@@ -178,7 +178,7 @@ class LeaderboardStore(AutoVerifiedRequestHandler):
                     res['streak'] = '%s%s' % (last_game, game_history.index(opposite) if  opposite in game_history else len(game_history))
                     if column == 'rating':
                         res['rank_change'] = (previous_ranking[res['id']][0]- i) if previous_ranking.has_key(res['id']) else None
-                    res['rating_change'] = (previous_ranking[res['id']][1]- res['rating']) if previous_ranking.has_key(res['id']) else None
+                    res['rating_change'] = (res['rating']-previous_ranking[res['id']][1]) if previous_ranking.has_key(res['id']) else None
 
                 data = "{}&& "+json.dumps(result)
                 self.set_header('Content-range', 'items {}-{}/{}'.format(start, stop, total))
